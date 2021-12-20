81b2fa0de78c066dbabadec212f30a3da970f62d
613ysj <1632810747@qq.com>
2021-12-20 16:11:36 +0800

    day05 广告同步实现(redis1)

--- changgou-parent/changgou-service/changgou-service-cannal/src/main/java/com/changgou/CanalApplication.java
+++ changgou-parent/changgou-service/changgou-service-cannal/src/main/java/com/changgou/CanalApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author ljh
@@ -16,6 +17,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
 @EnableCanalClient//启用canal的监听
+@EnableFeignClients(basePackages = "com.changgou.content.feign")//启用feign的注解生效 basePackages 指定feig接口所在的包路基
 public class CanalApplication {
     public static void main(String[] args) {
         SpringApplication.run(CanalApplication.class,args);
--- changgou-parent/changgou-service/changgou-service-cannal/src/main/java/com/changgou/canal/listener/MyEventListener.java
+++ changgou-parent/changgou-service/changgou-service-cannal/src/main/java/com/changgou/canal/listener/MyEventListener.java
@@ -1,7 +1,13 @@
 package com.changgou.canal.listener;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.changgou.content.feign.ContentFeign;
+import com.changgou.content.pojo.Content;
 import com.xpand.starter.canal.annotation.*;
+import entity.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.List;
 
@@ -41,12 +47,67 @@ public class MyEventListener {
    /* @DeleteListenPoint
     public void onEvent3(CanalEntry.EventType eventType) {
         System.err.println("DeleteListenPoint");
-    }
+    }*/
 
     //客制化，可以指定任意的类型（insert ,delete ,update ,create index ）
-    @ListenPoint(destination = "example", schema = "canal-test", table = {"t_user", "test_table"}, eventType = CanalEntry.EventType.UPDATE)
+    //当changgou_content数据库中的tb_content被别人 insert update delete的时候都需要监听
+
+    @Autowired
+    private ContentFeign contentFeign;
+
+    //字符串类型
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    //destination 指定要监听的目的地和配置文件中的值一致 和canal-server中的目录的名称一致
+    // schema  指定监听的库
+    // table 监听的表
+    // eventType 监听的操作类型
+    @ListenPoint(destination = "example",
+            schema = "changgou_content",
+            table = {"tb_content"},
+            eventType = {CanalEntry.EventType.UPDATE, CanalEntry.EventType.INSERT, CanalEntry.EventType.DELETE})
     public void onEvent4(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
-        System.err.println("DeleteListenPoint");
-        rowData.getAfterColumnsList().forEach((c) -> System.err.println("By--Annotation: " + c.getName() + " ::   " + c.getValue()));
-    }*/
+        //1.获取被修改的数据的行的category_id的字段对应的值
+        String columnValue = getColumnValue(eventType, rowData);
+        //2.调用feign 从广告微服务中获取最新的该分类下的所有的广告的列表
+        //2.1 加入起步依赖
+        //2.2 写一个接口@feignclient注解指定调用的服务的名称（name的值）
+        //2.3 写一个方法 和调用的时候 调用的服务的controller一致
+        //2.4 写一个方法实现接口（调用的服务方写一个controller来实现）
+
+        //2.5 使用feign方 需要添加feign依赖
+        //2.6 开启 feign 注入
+        Result<List<Content>> result = contentFeign.findByCategory(Long.valueOf(columnValue));
+        List<Content> contentList = result.getData();
+        //3.链接到redis中 将数据存储到redis覆盖
+        stringRedisTemplate.boundValueOps("content_"+columnValue).set(JSON.toJSONString(contentList));
+        //set key  value
+        }
+
+    //获取category_id的列的值
+    private String getColumnValue(CanalEntry.EventType eventType, CanalEntry.RowData rowData) {
+        String categoryId = "";
+        if (eventType == CanalEntry.EventType.DELETE) {
+            //获取删除之前的数据
+            List<CanalEntry.Column> beforeColumnsList = rowData.getBeforeColumnsList();
+            for (CanalEntry.Column column : beforeColumnsList) {
+                //如果是列名为category_id
+                if (column.getName().equals("category_id")) {
+                    categoryId = column.getValue();//分类的ID 的值    比如：1   2
+                    break;
+                }
+            }
+        } else {
+            //获取更新或者添加之后的数据
+            List<CanalEntry.Column> afterColumnsList = rowData.getAfterColumnsList();
+            for (CanalEntry.Column column : afterColumnsList) {
+                if (column.getName().equals("category_id")) {
+                    categoryId = column.getValue();//分类的ID 的值    比如：1   2
+                    break;
+                }
+            }
+        }
+        return categoryId;
+    }
 }
