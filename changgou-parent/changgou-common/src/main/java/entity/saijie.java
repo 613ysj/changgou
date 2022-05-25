442fe20ad39b3ed1720942be1974516ca5216134
613ysj <1632810747@qq.com>
2022-05-25 15:59:56 +0800

    saijie分支sec

---
+++ changgou-parent/changgou-common/src/main/java/entity/saijie.java
@@ -0,0 +1,12 @@
+package entity;
+
+/**
+ * @author: saijie
+ * @date: 2022/5/25 15:42
+ */
+public class saijie {
+    public static void main(String[] args) {
+        System.out.println("saijie");
+        System.out.println("master");
+    }
+}
